6957a7f99737a203cca7a7109d08e367bc9f0495
mhemanthkmr <m.hemanthkmr143@gmail.com>
2021-05-09 22:23:19 +0530

    opening camera and reading

--- Opencv.py
+++ Opencv.py
@@ -1,3 +1,9 @@
 #! /usr/bin/env python3
 
-import 
+import cv2
+camera = cv2.VideoCapture(0)
+while camera.isOpened():
+    ret, frame = camera.read()
+    if cv2.waitKey(10) == ord('q'):
+        break
+    cv2.imshow('Hemanth Camera',frame)
